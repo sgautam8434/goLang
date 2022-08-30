5c289e57618b62fa6147012ae060cc332f6156ef
Siddhant Gautam <43059326+sgautam8434@users.noreply.github.com>
2022-08-31 01:16:29 +0530

    more variables

--- main.go
+++ main.go
@@ -11,8 +11,13 @@ func main() {
 	fmt.Println("interger", i)
 	myFunctionReturnVal := myFunc()
 	fmt.Println("call function", myFunctionReturnVal)
+	var1, var2 := returnTwoValues()
+	fmt.Println("2 values", var1, var2)
 }
 
 func myFunc() string {
 	return "myFunction"
+}
+func returnTwoValues() (string, string) {
+	return "firstString", "secondString"
 }
